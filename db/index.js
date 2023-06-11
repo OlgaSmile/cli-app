4f98704d737283d96fb785822b3716a2465b4279
Olga <Mamillaris21@gmail.com>
2023-06-11 19:46:57 +0300

    finished first task

---
+++ db/index.js
@@ -0,0 +1,61 @@
+const fs = require('fs').promises;
+const{ nanoid} = require('nanoid');
+
+const path = require('path');
+
+const contactsPath = path.join(__dirname, "contacts.json");
+
+
+// TODO: задокументувати кожну функцію
+async function listContacts() {
+  const result = await fs.readFile(contactsPath);
+  return JSON.parse(result);
+  }
+  
+ async function getContactById(contactId) {
+    const contacts = await listContacts();
+    const contactToFind = contacts.find(contact=>contact.id===contactId);
+
+    return contactToFind || null;
+  }
+  
+  async function removeContact(contactId) {
+    const contacts = await listContacts();
+
+    const index = contacts.findIndex(contact=>contact.id===contactId);
+
+    if(index===-1){
+      return null
+    };
+    
+    const [result] = contacts.splice(index, 1);
+
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+    return result;
+  }
+  
+   async function addContact(name, email, phone) {
+    const contacts = await listContacts();
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone
+    };
+
+    contacts.push(newContact);
+
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+    return newContact;
+
+  }
+
+  module.exports={
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+  }
